8810ec94362939fe524944df21f6a186fbc16c8f
Đặng Quang Linh <linhlay2809@gmail.com>
2021-07-13 00:42:16 +0700

    Add Slither Game to repository

---
+++ Slither.io/Assets/MenuScript/OpenAds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class OpenAds : MonoBehaviour
+{
+	public void ShowAd()
+	{
+//		if (Advertisement.IsReady())
+//		{
+//			Advertisement.Show();
+//		}
+
+
+
+	}
+}
+
---
+++ Slither.io/Assets/MenuScript/ShowScore.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour {
+	Text FinalScoreText, BestScoreText;
+	InputField nickname;
+	Button removeAds, showAds;
+	// Use this for initialization
+	void Start () {
+		string final, best;
+		int finalScore,bestScore;
+
+		final = "Y o u r   f i n a l   l e n g t h   w a s  ";
+		best = "Y o u r   b e s t   l e n g t h   e v e r   w a s  ";
+		//Get the final score and best score from game.
+		finalScore = PlayerPrefs.GetInt ("FinalScore", -1);
+		bestScore = PlayerPrefs.GetInt ("BestScore", -1);
+		//Get the target UI
+		FinalScoreText = GameObject.Find("final").GetComponent<Text>();
+		BestScoreText = GameObject.Find("bst").GetComponent<Text>();
+		nickname = GameObject.Find("Nickname").GetComponent<InputField>();
+
+
+		//Keep showing the nickname after game ends.
+		nickname.text = PlayerPrefs.GetString ("nickname","");	
+		//Do not show final score and best score when app is just opened.
+		if (finalScore == -1) {
+			FinalScoreText.enabled = false;
+			BestScoreText.enabled = false;
+		} else {
+		//Update the best score if final score is greater than last best score.
+			FinalScoreText.text = final + finalScore;
+			BestScoreText.text = best + bestScore;
+		}
+
+
+
+
+	}
+}
